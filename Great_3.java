e61451df652d2d4372cbcdf24ada99d4e853944d
dhineshwar balachandar <dhineshdhanush77@gmail.com>
2021-11-26 16:17:39 +0530

    First commit

---
+++ Great_3.java
@@ -0,0 +1,24 @@
+package hellowrold1;
+
+public class Great_3 {
+
+	public static void main(String[] args) {
+	
+		int a = 2650;
+		int b = 3000;
+		int c = 2500;
+		    
+		    if (a>b && a>c)
+		    
+		    {
+		        System.out.println(a+ " is the greatest");
+		    }
+		    else if (b>c && b>a)
+		    {
+		        System.out.print(b+ " is the greatest");
+		    }else
+		        System.out.print(c+ " is the greatest");
+
+	}
+
+}
